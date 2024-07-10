545116375d2b9e1d855b63ed2383507d7f7d25d8
Daniel Cardenas <gaarauzumaki23@gmail.com>
2024-07-09 22:29:58 -0400

    Update WME Layer Counter.js
    Fixed Code

--- WME Layer Counter.js
+++ WME Layer Counter.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Layer Counter
 // @namespace    https://greasyfork.org/en/scripts/476456-wme-layer-counter
-// @version      2024.7.9.4
+// @version      2024.7.9.5
 // @description  See how many layers you have active in WME.
 // @match        *://*.waze.com/*editor*
 // @exclude      *://*.waze.com/user/editor*
@@ -17,7 +17,7 @@
     'use strict';
 
     const SCRIPT_NAME = 'WME Layer Counter';
-    const updateMessage = "<b>Changelog</b><br><br>Update 2024.7.9.4<br>- Moved layer counter over to the tab section. If you prefer, you can still have it show on the top right.<br><br>Update 2024.7.9.1<br>- Removed colors.<br><br>Update 2024.7.4.2<br>- Added dynamic button color change based on active layers.<br><br>Update 2024.7.8.1<br>- Updated the way max layers are calculated.<br><br>Update 2023.10.4.8<br>- Found out Layer Counter wanted to hangout with the cool kids from the FUME block. I scolded him and told him he can't hangout with them. I then sent him to the corner and told him not to move again.<br><br>Update 2023.10.4.5<br>- Fixed no display issue.<br><br>Initial Release.<br>- Hope this helps those that need to know how many layers they are using.<br><br>";
+    const updateMessage = "<b>Changelog</b><br><br>Update 2024.7.9.5<br>- Fixed code.<br><br>Update 2024.7.9.3<br>- Moved layer counter over to the tab section. If you prefer, you can still have it show on the right.<br><br>Update 2024.7.9.1<br>- Removed colors.<br><br>Update 2024.7.4.2<br>- Added dynamic button color change based on active layers.<br><br>Update 2024.7.8.1<br>- Updated the way max layers are calculated.<br><br>Update 2023.10.4.8<br>- Found out Layer Counter wanted to hangout with the cool kids from the FUME block. I scolded him and told him he can't hangout with them. I then sent him to the corner and told him not to move again.<br><br>Update 2023.10.4.5<br>- Fixed no display issue.<br><br>Initial Release.<br>- Hope this helps those that need to know how many layers they are using.<br><br>";
     const scriptVersion = GM_info.script.version;
 
     let _$layerCountElem = null;
@@ -42,16 +42,9 @@
         if (!_$layerCountElem) {
             createLayerCountElement();
         }
-        const activeLayers = W.map.olMap.layers.filter(layer => layer.visibility).length;
-        const featureLayer = W.map.olMap.Z_INDEX_BASE.Feature;
-        const overlayLayer = W.map.olMap.Z_INDEX_BASE.Overlay;
-        const maxLayers = (featureLayer - overlayLayer) / 5;
-
-        const layerCountText = `${activeLayers}/${maxLayers}`;
-
-        // Set layer count text
+        const activeLayersTextForButton = document.getElementById('active-layers-button').textContent;
         const itemContainer = _$layerCountElem.querySelector('.item-container');
-        itemContainer.textContent = layerCountText;
+        itemContainer.textContent = activeLayersTextForButton;
     }
 
     function injectLayerCountElement() {
@@ -105,6 +98,11 @@
         activeLayersText.id = 'active-layers';
         tabPane.appendChild(activeLayersText);
 
+        const activeLayersTextForButton = document.createElement('p');
+        activeLayersTextForButton.id = 'active-layers-button';
+        activeLayersTextForButton.style.display = 'none';
+        tabPane.appendChild(activeLayersTextForButton);
+
         const createdLayersText = document.createElement('p');
         createdLayersText.id = 'created-layers';
         tabPane.appendChild(createdLayersText);
@@ -123,14 +121,17 @@
         version.style.margin = '0';
         tabPane.appendChild(version);
 
-        updateTabContent(activeLayersText, createdLayersText, maxLayersText);
+        updateTabContent(activeLayersText, activeLayersTextForButton, createdLayersText, maxLayersText);
 
         setInterval(() => {
-            updateTabContent(activeLayersText, createdLayersText, maxLayersText);
+            updateTabContent(activeLayersText, activeLayersTextForButton, createdLayersText, maxLayersText);
+            if (_isButtonVisible) {
+                updateLayerCount();
+            }
         }, 1000);
     }
 
-    function updateTabContent(activeLayersText, createdLayersText, maxLayersText) {
+    function updateTabContent(activeLayersText, activeLayersTextForButton, createdLayersText, maxLayersText) {
         const activeLayers = W.map.olMap.layers.filter(layer => layer.visibility).length;
         const createdLayers = W.controller.map.layers.length;
         const featureLayer = W.map.olMap.Z_INDEX_BASE.Feature;
@@ -138,20 +139,54 @@
         const maxLayers = (featureLayer - overlayLayer) / 5;
 
         activeLayersText.textContent = `Active Layers: ${activeLayers}`;
+        activeLayersTextForButton.textContent = `${activeLayers}/${maxLayers}`;
         createdLayersText.textContent = `Created Layers: ${createdLayers}`;
         maxLayersText.textContent = `Max Layers: ${maxLayers}`;
     }
 
+    function observeLayerChanges() {
+        const layersContainer = document.querySelector('.layer-switcher');
+        if (layersContainer) {
+            const observer = new MutationObserver(() => {
+                updateTabContent(
+                    document.getElementById('active-layers'),
+                    document.getElementById('active-layers-button'),
+                    document.getElementById('created-layers'),
+                    document.getElementById('max-layers')
+                );
+                if (_isButtonVisible) {
+                    updateLayerCount();
+                }
+            });
+
+            observer.observe(layersContainer, {
+                childList: true,
+                subtree: true,
+                attributes: true,
+                attributeFilter: ['style', 'class']
+            });
+        } else {
+            console.error('Layer container not found');
+        }
+    }
+
     // Initialize the script
     function initialize() {
+        if (typeof W === 'undefined' || typeof WazeWrap === 'undefined') {
+            console.log('WazeWrap or W is not available. Exiting script.');
+            return;
+        }
+
         if (W?.userscripts?.state.isReady) {
             addScriptTab();
             injectLayerCountElement();
+            observeLayerChanges();
             showScriptUpdate();
         } else {
             document.addEventListener('wme-ready', function () {
                 addScriptTab();
                 injectLayerCountElement();
+                observeLayerChanges();
                 showScriptUpdate();
             }, { once: true });
         }
@@ -162,13 +197,17 @@
 
     // Show script update notification
     function showScriptUpdate() {
-        WazeWrap.Interface.ShowScriptUpdate(
-            SCRIPT_NAME,
-            GM_info.script.version,
-            updateMessage,
-            'https://greasyfork.org/en/scripts/476456-wme-layer-counter',
-            'https://www.waze.com/forum/viewtopic.php?t=394699'
-        );
+        try {
+            WazeWrap.Interface.ShowScriptUpdate(
+                SCRIPT_NAME,
+                GM_info.script.version,
+                updateMessage,
+                'https://greasyfork.org/en/scripts/476456-wme-layer-counter',
+                'https://www.waze.com/forum/viewtopic.php?t=394699'
+            );
+        } catch (error) {
+            console.error('Error showing script update:', error);
+        }
     }
 
 })();
